1f258fbdcc4ce6be3ddf51a0a7f20389a4927b1c
Mewr11 <mewr.11.coding@gmail.com>
2018-04-27 22:58:17 -0500

    Changed the mcmod.info and related information.

--- src/main/java/com/ultima/coe/Reference.java
+++ src/main/java/com/ultima/coe/Reference.java
@@ -2,8 +2,8 @@ package com.ultima.coe;
 
 public class Reference {
 	
-	public static final String MOD_ID = "ctech";
-	public static final String NAME = "CalTech";
+	public static final String MOD_ID = "coe";
+	public static final String NAME = "Children of Earth";
 	public static final String VERSION = "0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2,1.13)";
 
