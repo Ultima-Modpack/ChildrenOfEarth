bde952f19cdcf0fd9fde691100aa238f7f203307
Mewr11 <mewr.11.coding@gmail.com>
2018-04-28 18:50:59 -0500

    Campfire now has an associated TileEntity

--- src/main/java/com/ultima/coe/block/basic/campfire/CampfireBlock.java
+++ src/main/java/com/ultima/coe/block/basic/campfire/CampfireBlock.java
@@ -1,14 +1,46 @@
 package com.ultima.coe.block.basic.campfire;
 
-import com.ultima.coe.block.BlockBase;
+import javax.annotation.Nullable;
+
+import com.ultima.coe.block.BlockTileEntity;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class CampfireBlock extends BlockBase {
+public class CampfireBlock extends BlockTileEntity<CampfireTileEntity> {
 	
 	public CampfireBlock() {
 		super(Material.FIRE, "campfire");
+	}
+	
+	@Override
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand,
+			EnumFacing side, float hitX, float hitY, float hitZ) {
+		
+		if(!world.isRemote) {
+			CampfireTileEntity tile = getTileEntity(world, pos);
+			// TODO process the tileEntity
+			tile.equals(tile);
+		}
+		
+		return true;
+		
+	}
+	
+	@Override
+	public Class<CampfireTileEntity> getTileEntityClass() {
+		return CampfireTileEntity.class;
+	}
+	
+	@Nullable
+	@Override
+	public CampfireTileEntity createTileEntity(World world, IBlockState state) {
+		return new CampfireTileEntity();
 	}
 	
 	@Override
@@ -22,5 +54,4 @@ public class CampfireBlock extends BlockBase {
 	public boolean isFullCube(IBlockState state) {
 		return false;
 	}
-	
 }
---
+++ src/main/java/com/ultima/coe/block/basic/campfire/CampfireTileEntity.java
@@ -0,0 +1,9 @@
+package com.ultima.coe.block.basic.campfire;
+
+import net.minecraft.tileentity.TileEntity;
+
+public class CampfireTileEntity extends TileEntity {
+	
+	// TODO write our Tile Entity class
+	
+}
