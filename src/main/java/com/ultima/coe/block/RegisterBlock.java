04636d7c592c3487a5be8662841667d3974ad108
Mewr11 <mewr.11.coding@gmail.com>
2018-04-28 16:33:15 -0500

    Started work on the campfire

--- src/main/java/com/ultima/coe/block/RegisterBlock.java
+++ src/main/java/com/ultima/coe/block/RegisterBlock.java
@@ -1,21 +1,29 @@
 package com.ultima.coe.block;
 
+import com.ultima.coe.block.campfire.CampfireBlock;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class RegisterBlock {
 	
+	public static CampfireBlock campfire = new CampfireBlock();
+	
 	public static void register(IForgeRegistry<Block> registry) {
-		
+		registry.registerAll(
+				campfire
+		);
 	}
 	
 	public static void registerItemBlocks(IForgeRegistry<Item> registry) {
-		
+		registry.registerAll(
+				campfire.createItemBlock()
+		);
 	}
 	
 	public static void registerModels() {
-		
+		campfire.registerItemModel(Item.getItemFromBlock(campfire));
 	}
 	
 }
---
+++ src/main/java/com/ultima/coe/block/campfire/CampfireBlock.java
@@ -0,0 +1,26 @@
+package com.ultima.coe.block.campfire;
+
+import com.ultima.coe.block.BlockBase;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+
+public class CampfireBlock extends BlockBase {
+	
+	public CampfireBlock() {
+		super(Material.FIRE, "campfire");
+	}
+	
+	@Override
+	@Deprecated
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+	
+	@Override
+	@Deprecated
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+	
+}
