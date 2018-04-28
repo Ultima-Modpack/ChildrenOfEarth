cab2365140133d63cf2910d8cca8ca34805cd636
calebrudolph <calebrudolph@gmail.com>
2018-04-27 22:28:47 -0500

    Missing Import added

--- src/main/java/com/ultima/coe/ChildrenOfEarth.java
+++ src/main/java/com/ultima/coe/ChildrenOfEarth.java
@@ -1,5 +1,38 @@
 package com.ultima.coe;
 
-public class ChildrenOfEarth {
+import java.util.logging.Logger;
 
+import com.ultima.coe.proxy.CommonProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS, dependencies = Reference.DEPENDENCIES)
+public class ChildrenOfEarth {
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+	public static CommonProxy proxy;
+
+	public static Logger logger;
+
+	@Mod.Instance
+	public static ChildrenOfEarth instance;
+
+	// Item and Block Initialization
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		proxy.preInit(event);
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		proxy.init(event);
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		proxy.postInit(event);
+	}
 }
