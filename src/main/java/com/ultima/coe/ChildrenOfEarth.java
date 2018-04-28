407a99c51e6bf5209b66374d13d3c4b5598f027f
Mewr11 <mewr.11.coding@gmail.com>
2018-04-28 15:58:06 -0500

    Added in a block and item registry framework

--- src/main/java/com/ultima/coe/ChildrenOfEarth.java
+++ src/main/java/com/ultima/coe/ChildrenOfEarth.java
@@ -2,13 +2,20 @@ package com.ultima.coe;
 
 import java.util.logging.Logger;
 
+import com.ultima.coe.block.RegisterBlock;
+import com.ultima.coe.item.RegisterItem;
 import com.ultima.coe.proxy.CommonProxy;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS)
 public class ChildrenOfEarth {
@@ -21,6 +28,27 @@ public class ChildrenOfEarth {
 	public static ChildrenOfEarth instance;
 
 	// Item and Block Initialization
+	@Mod.EventBusSubscriber
+	public static class RegistrationHandler {
+		
+		@SubscribeEvent
+		public static void registerBlocks(RegistryEvent.Register<Block> event) {
+			RegisterBlock.register(event.getRegistry());
+		}
+		
+		@SubscribeEvent
+		public static void registerItems(RegistryEvent.Register<Item> event) {
+			RegisterItem.register(event.getRegistry());
+		}
+		
+		@SubscribeEvent
+		public static void registerModels(ModelRegistryEvent event) {
+			RegisterBlock.registerModels();
+			RegisterItem.registerModels();
+		}
+		
+	}
+	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		proxy.preInit(event);
---
+++ src/main/java/com/ultima/coe/item/RegisterItem.java
@@ -0,0 +1,16 @@
+package com.ultima.coe.item;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.IForgeRegistry;
+
+public class RegisterItem {
+	
+	public static void register(IForgeRegistry<Item> registry) {
+		
+	}
+	
+	public static void registerModels() {
+		
+	}
+	
+}
